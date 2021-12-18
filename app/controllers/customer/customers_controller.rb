e1dbe2667ad402fe9002a7e4a899395b8fb1071b
t-nabe07 <watanabe.t.job@gmail.com>
2021-12-18 07:08:00 +0000

    nabe

---
+++ app/controllers/customer/customers_controller.rb
@@ -0,0 +1,18 @@
+class Customer::CustomersController < ApplicationController
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def withdraw #退会処理
+  end
+
+  def unsubscribe #退会画面表示
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'customers/edit'
+  get 'customers/show'
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords,], controllers: {
