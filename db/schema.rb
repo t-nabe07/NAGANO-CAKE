dbdf4b2eacc1919e9672af434d925bc3e04ab253
oreorange <92431652+oreorange@users.noreply.github.com>
2021-12-20 07:37:56 +0000

    hige

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_12_20_014200) do
+ActiveRecord::Schema.define(version: 2021_12_20_060843) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
