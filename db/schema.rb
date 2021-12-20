5ca61ffe02575b9922b19eb008a7ef6912acd5cb
P-manBrown <tachikawa.yuya@outlook.jp>
2021-12-20 07:22:01 +0000

    [add] about

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_12_18_132234) do
+ActiveRecord::Schema.define(version: 2021_12_19_031614) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
