9566ea62bd77ede7f9ec7ad30e2b3ed97a0b05ee
Makikotakemori <t_makiko_1996@yahoo.co.jp>
2021-12-21 04:11:09 +0000

    [add]customer_items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
   
   def index
-    @items = Item.all
+    @items = Item.page(params[:page]).per(10)
   end
   
   def show
+    @item = Item.find(params[:id])
   end
 end
