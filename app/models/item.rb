f1d69bbb552434e14e05ebb5d9c04fb9fff93285
oreorange <92431652+oreorange@users.noreply.github.com>
2021-12-23 05:23:48 +0000

    hoge

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
 
  attachment :image
  belongs_to :genre
+
+ has_many :cart_items, dependent: :destroy
+ has_many :order_items, dependent: :destroy
+ 
+ def add_tax_sales_price
+  (self.price * 1.10).round
+ end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 2021_12_20_060843) do
 
 
+
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
