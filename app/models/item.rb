a46ca2111c8f130576daf87596c70192cc21bd35
Makikotakemori <t_makiko_1996@yahoo.co.jp>
2021-12-24 07:19:14 +0000

    [finish]customer_order_layout

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
 
  has_many :cart_items, dependent: :destroy
  has_many :order_items, dependent: :destroy
+ has_many :orders
 
  def add_tax_sales_price
   (self.price * 1.10).round
